41c9c86a96120039d950954c16cffceb6f748c37
Darth Degen <darthdegen@gmail.com>
2024-07-30 07:12:46 -0700

    improve landing page asset load

--- src/components/templates/LandingView.tsx
+++ src/components/templates/LandingView.tsx
@@ -3,6 +3,8 @@ import { handleAssetLoad } from "@utils";
 import Image from "next/image";
 import { ViewContext } from "@contexts";
 import { useWindowSize } from "src/hooks";
+import { enterAnimation } from "src/constants";
+import { motion } from "framer-motion";
 
 interface Props {
   setAssets: Dispatch<SetStateAction<boolean[]>>;
@@ -14,23 +16,32 @@ const LandingView: FC<Props> = (props: Props) => {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <Image
-        src="/images/icons/logo-text.svg"
-        width={814}
-        height={106}
-        alt="Hyblinxx"
+      <motion.div
+        {...enterAnimation}
         className="absolute-center !top-[40svh] z-10 px-5 md:px-10 lg:px-0"
-      />
-      <Image
-        src={`/images/pages/landing/clouds-${
-          winwWidth >= 768 ? "xl" : "sm"
-        }.png`}
-        alt="EXP"
-        width={1439}
-        height={783}
-        className="object-contain w-screen absolute bottom-0 2xl:-bottom-32 3xl:-bottom-[25vh] -z-10"
-        onLoadingComplete={() => handleAssetLoad(0, setAssets)}
-      />
+      >
+        <Image
+          src="/images/icons/logo-text.svg"
+          width={814}
+          height={106}
+          alt="Hyblinxx"
+        />
+      </motion.div>
+      <motion.div
+        {...enterAnimation}
+        className="object-contain fixed bottom-0 2xl:-bottom-32 3xl:-bottom-[25vh] -z-10"
+      >
+        <Image
+          src={`/images/pages/landing/clouds-${
+            winwWidth >= 768 ? "xl" : "sm"
+          }.png`}
+          alt="EXP"
+          width={1439}
+          height={783}
+          className="w-screen"
+          onLoadingComplete={() => handleAssetLoad(0, setAssets)}
+        />
+      </motion.div>
     </div>
   );
 };
